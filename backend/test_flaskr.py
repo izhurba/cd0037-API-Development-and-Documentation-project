863a9069f70a38a56efc265666742dc008756ce1
Ilya Zhurba <i.n.zhurba@gmail.com>
2022-04-19 20:52:13 -0600

    Adding more tests

--- backend/test_flaskr.py
+++ backend/test_flaskr.py
@@ -1,6 +1,7 @@
 import os
 import unittest
 import json
+from urllib import response
 from flask_sqlalchemy import SQLAlchemy
 
 from flaskr import create_app
@@ -34,9 +35,47 @@ class TriviaTestCase(unittest.TestCase):
     Write at least one test for each test for successful operation and for expected errors.
     """
 
-    def test_paginate_questions(self):
-        response = self.client().get('/questions')
-        data = json.loads(response.data)
+    def test_get_categories(self):
+        resp = self.client().get('/catagories')
+        data = json.loads(resp.data)
+
+        self.assertEqual(resp.status_code, 200)
+        self.assertEqual(data['success'], True)
+        self.assertEqual(len(data['categories']), 6)
+
+    def test_get_questions(self):
+        resp = self.client().get('/questions')
+        data = json.loads(resp.data)
+
+        self.assertEqual(resp.status_code, 200)
+        self.assertEqual(data['success'], True)
+        self.assertEqual(data['total_questions'], 19)
+        self.assertEqual(len(data['questions']), 10)
+        self.assertEqual(data['questions'][0]['id'], 5)
+    
+    def test_get_questions_404(self):
+        resp = self.client().get('/questions?page=10')
+        self.assertEqual(resp.status_code, 404)
+    
+    def test_get_questions_by_category(self):
+        resp = self.client().get('/questions?page=1&category_id=3')
+        data = json.loads(resp.data)
+
+        self.assertEqual(resp.status_code, 200)
+        self.assertEqual(data['current_category'], 3)
+        self.assertEqual(data['total_questions'], 3)
+
+    def test_get_questions_by_category_400(self):
+        resp = self.client().get('/questions?page=1&category_id=11')
+        self.assertEqual(resp.status_code, 400)
+    
+    def test_search_questions(self):
+        resp = self.client().get('/questions?page=1&search_term="penicillin"')
+        data = json.loads(resp.data)
+
+        self.assertEqual(data['success'], True)
+        self.assertEqual(len(data['questions']), 1)
+        self.assertEqual(data['questions'][0]['id'], 21)
 
 
 
